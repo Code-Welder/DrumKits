b5fde95ba872cf3179c2a8ef5c271888731faae0
Vadim B <vadim.bobrovitsky@gmail.com>
2021-02-25 16:45:40 +0300

    code reorganization

--- main.js
+++ main.js
@@ -1,72 +1,66 @@
-const selectors = document.querySelectorAll('.sound-selector')
-const drums     = document.querySelectorAll('div[data-key^="Key"]')
+const selectors = document.querySelectorAll('.sound-selector');
+const drums     = document.querySelectorAll('div[data-key^="Key"]');
 
-// ** Keyboard controll **
+selectors.forEach( node => {
+  volume_change(node);
+  sound_switcher(node);
+});
 
-document.addEventListener('keydown', (e) => {
-  const drum  = document.querySelector(`div[data-key="${e.code}"]`)
-  const audio = document.querySelector(`audio[data-key="${e.code}"]`)
+drums.forEach( drum => {
+  touch_play(drum);
+});
+
+document.addEventListener('keydown', play_audio)
+
+// **********
+
+function play_audio(event) {
+  const drum  = document.querySelector(`div[data-key="${event.code}"]`);
+  const audio = document.querySelector(`audio[data-key="${event.code}"]`);
 
   if(!drum) return;
 
-    play_hit(audio)
+    audio.currentTime = 0;
+    audio.play();
 
-    drum.classList.add('drum-play')
+    drum.classList.add('touched') ;
     drum.addEventListener('transitionend', (e) => {
+      if(e.propertyName != 'transform') return;
+      drum.classList.remove('touched')
+    });
+}
 
-         if(e.propertyName != 'transform') return
-         drum.classList.remove('drum-play')
-  })
-})
+function volume_change(node) {
+  const range = node.querySelector('input[type="range"]');
+  const sound = node.querySelector('audio');
 
-selectors.forEach( selector => {
-  swap_hit(selector)
-  change_volume(selector)
-})
+  if(!range) return;
 
-drums.forEach( drum => {
-  touch_play(drum)
-})
+  range.onchange = function() {
+    sound.volume = this.value;
+  };
+}
 
-// ** mobile **
+function sound_switcher(node) {
+  const sound  = node.querySelector('audio');
+  const select = node.querySelector('select');
+
+  select.onchange = function() {
+    sound.src = this.value;
+  };
+}
 
 function touch_play(drum) {
   drum.onpointerdown = function() {
-    const sound = document.querySelector(`audio[data-key="${this.dataset.key}"]`)
-    play_hit(sound)
+    const sound = document.querySelector(`audio[data-key="${drum.dataset.key}"]`);
 
-    drum.classList.add('drum-play')
+          sound.currentTime = 0;
+          sound.play();
+
+    drum.classList.add('touched')
     drum.addEventListener('transitionend', (e) => {
-
-         if(e.propertyName != 'transform') return
-         drum.classList.remove('drum-play')
-    })
-  }
-}
-
-// ** fn **
-
-function play_hit(audio) {
-  audio.currentTime = 0
-  audio.play()
-}
-
-function swap_hit(el) {
-  const sound    = el.querySelector('audio')
-  const selector = el.querySelector('select')
-
-  selector.onchange = function() {
-    sound.src = this.value
-  }
-}
-
-function change_volume(el) {
-  const input = el.querySelector('input[type="range"')
-  const sound = el.querySelector('audio')
-
-  if(!input) return;
-
-  input.onchange = function() {
-    sound.volume = this.value
+      if(e.propertyName != 'transform') return
+      drum.classList.remove('touched')
+    });
   }
 }
